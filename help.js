0d285f218c9b36118d430b7cc2d028073c061387
MR-NC <61655493+MR-NC@users.noreply.github.com>
2021-01-08 22:50:45 +0700

    Add files via upload

---
+++ help.js
@@ -0,0 +1,98 @@
+
+const help = (prefix) => { 
+	return `                 
+┏━━❉ *About Bot* ❉━━┓
+┣⊱ *${prefix}owner*
+┣⊱ *${prefix}donasi*
+┣⊱ *${prefix}info*
+┣⊱ *${prefix}botstat*
+┣⊱ *${prefix}laporadmin*
+┣━━❀ *Maker* ❀━━
+┣⊱ *${prefix}ocr*
+┣⊱ *${prefix}sticker*
+┣⊱ *${prefix}tts*
+┣⊱ *${prefix}toimg*
+┣⊱ *${prefix}phlogo*
+┣⊱ *${prefix}quotemaker*
+┣⊱ *${prefix}ninjalogo*
+┣⊱ *${prefix}wolflogo*
+┣⊱ *${prefix}lionlogo*
+┣⊱ *${prefix}glitch*
+┣⊱ *${prefix}text3d*
+┣━━❀ *Random Picture* ❀━━
+┣⊱ *${prefix}meme*
+┣⊱ *${prefix}memeindo*
+┣⊱ *${prefix}randomloli*
+┣⊱ *${prefix}randomneko*
+┣⊱ *${prefix}randomanime*
+┣⊱ *${prefix}randompokemon*
+┣⊱ *${prefix}randomcars*
+┣⊱ *${prefix}randomanjing*
+┣⊱ *${prefix}randomkucing*
+┣⊱ *${prefix}randomaesthetic*
+┣━━❀ *Kerang ajaib* ❀━
+┣⊱ *${prefix}kapankah*
+┣⊱ *${prefix}apakah*
+┣⊱ *${prefix}bisakah*
+┣⊱ *${prefix}rate*
+┣━━❀ *Anime* ❀━━
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}nsfwloli*
+┣⊱ *${prefix}nsfwblowjob*
+┣⊱ *${prefix}nsfwneko*
+┣⊱ *${prefix}nsfwtrap*
+┣━━❀ *Media* ❀━━
+┣⊱ *${prefix}tiktokstalk*
+┣⊱ *${prefix}ytmp4*
+┣━━❀ *Other* ❀━━
+┣⊱ *${prefix}ping*
+┣⊱ *${prefix}shorturl*
+┣⊱ *${prefix}fototiktok*
+┣⊱ *${prefix}map*
+┣⊱ *${prefix}kbbi*
+┣⊱ *${prefix}artinama*
+┣⊱ *${prefix}infogempa*
+┣⊱ *${prefix}hilih*
+┣⊱ *${prefix}bucin*
+┣⊱ *${prefix}persengay*
+┣⊱ *${prefix}quotes*
+┣⊱ *${prefix}cerpen*
+┣⊱ *${prefix}truth*
+┣⊱ *${prefix}dare*
+┣⊱ *${prefix}lirik*
+┣⊱ *${prefix}chord*
+┣⊱ *${prefix}hidetag*
+┣⊱ *${prefix}ssweb*
+┣⊱ *${prefix}indohot*
+┣━━❀ *Group* ❀━━
+┣⊱ *${prefix}linkgc*
+┣⊱ *${prefix}tagall*
+┣⊱ *${prefix}add* [+62xx]
+┣⊱ *${prefix}group* [buka/tutup]
+┣⊱ *${prefix}demote* [tag]
+┣⊱ *${prefix}promote* [tag]
+┣⊱ *${prefix}kick* [62xx]
+┣⊱ *${prefix}listadmin*
+┣⊱ *${prefix}simih* [1/0]
+┣⊱ *${prefix}nsfw* [1/0]
+┣⊱ *${prefix}welcome* [1/0]
+┣━━❀ *Owner bot* ❀━
+┣⊱ *${prefix}clearall*
+┣⊱ *${prefix}blocklist*
+┣⊱ *${prefix}block*
+┣⊱ *${prefix}unblock*
+┣⊱ *${prefix}bc*
+┣⊱ *${prefix}leave*
+┣⊱ *${prefix}setpp*
+┣⊱ *${prefix}setprefix*
+┣━━━━━━━━━━━━━━
+┃*POWERED BY AINEBOT*
+┗━━━━━━━━━━━━━━
+
+`
+}
+exports.help = help
+
+
+
+  
